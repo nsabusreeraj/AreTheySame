06b106859a54416334521f73d07db46ba45b35da
nsabusreeraj <nsabusreeraj@salesforce.com>
2016-12-18 23:22:03 +0000

    aaaa

---
+++ AreTheySame/AreTheySame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AreTheySame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class AreTheySame
+    {
+        public static bool Comp(int[] a, int[] b)
+        {
+            bool result = false;
+            if (b == null || a == null)
+                return false;
+            if (a.Length != b.Length)
+                return false;
+            if (a.Length == 0 && b.Length == 0)
+                return true;
+
+            Array.Sort(a);
+            Array.Sort(b);
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(b[i] + "," + a[i]);
+                result = false;
+                if (a[i]*a[i] == Math.Abs(b[i]))
+                    result = true;
+
+                if (result == false)
+                    return false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AreTheySame/AreTheySameTests/AreTheySameTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AreTheySameTests
+{
+    [TestClass()]
+    public class AreTheySameTests
+    {
+        [TestMethod()]
+        public void CompTest1()
+        {
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 121, 14641, 20736, 361, 25921, 361, 20736, 361 };
+            bool result = AreTheySame.AreTheySame.Comp(a,b);
+            Assert.AreEqual(true,result);
+        }
+        [TestMethod()]
+        public void CompTest2()
+        {
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            bool result = AreTheySame.AreTheySame.Comp(a, b);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod()]
+        public void CompTest3()
+        {
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 132, 14641, 20736, 361, 25921, 361, 20736, 361 };
+            bool result = AreTheySame.AreTheySame.Comp(a, b);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void CompTest4()
+        {
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 121, 14641, 20736, 36100, 25921, 361, 20736, 361 };
+            bool result = AreTheySame.AreTheySame.Comp(a, b);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void CompTest5()
+        {
+            int[] a = new int[] {2,2,3 };
+            int[] b = new int[] {4,9,9 };
+            bool result = AreTheySame.AreTheySame.Comp(a, b);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void CompTest6()
+        {
+            int[] a = new int[] {  };
+            int[] b = new int[] {  };
+            bool result = AreTheySame.AreTheySame.Comp(a, b);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod()]
+        public void CompTest7()
+        {
+            bool result = AreTheySame.AreTheySame.Comp(null, null);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
